18b958f2f538c85b9b7ab938168a2da20a8c16f3
Sufiyan1803 <160001844+Sufiyan1803@users.noreply.github.com>
2024-07-24 16:50:58 +0530

    Learned Info about tuples

---
+++ 06_Tuples/tuple.py
@@ -0,0 +1,20 @@
+tea_types=("masala", "lemon","ginger")
+print(tea_types)
+
+
+len(tea_types)
+
+
+
+more_tea=("herbal", "earl grey")
+all_tea=more_tea + tea_types
+print(all_tea)
+
+
+
+tea_types=("masala", "lemon","ginger")
+(MASALA,LEMON,GINGER)=tea_types
+print(MASALA)
+print(LEMON)
+print(GINGER)
+
